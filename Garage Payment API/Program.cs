40f10713fc454f896f369e327ecf789972c87073
AbdoOo20 <abdoemad2082001@gmail.com>
2024-11-22 06:58:32 +0200

    sw

--- Garage Payment API/Program.cs
+++ Garage Payment API/Program.cs
@@ -36,7 +36,12 @@ namespace Garage_Payment_API
             //}
 
             app.UseSwagger();
-            app.UseSwaggerUI();
+            //app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                c.RoutePrefix = string.Empty; // Make Swagger accessible at the root of the API
+            });
 
             app.UseAuthorization();
 
